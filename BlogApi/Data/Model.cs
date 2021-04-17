717b55c8f3bd2a1c2163a985162d289358a4e75c
Eric Kerst <eric.kerst@gmail.com>
2021-04-17 18:24:45 +0200

    Context from protobuf

---
+++ BlogApi/Data/Model.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApi.Data
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        public BloggingContext()
+        {
+        }
+
+        public BloggingContext(DbContextOptions<BloggingContext> options) 
+            : base(options)
+        {
+        }
+
+        // The following configures EF to create a Sqlite database file as `C:\blogging.db`.
+        // For Mac or Linux, change this to `/tmp/blogging.db` or any other absolute path.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(@"Server=localhost;Database=blogging;User Id=sa;Password=gN7quwVBof5BEMPpwQLU;");
+    }
+}
--- BlogApi/Services/BlogService.cs
+++ BlogApi/Services/BlogService.cs
@@ -2,25 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlogApi.Data;
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace BlogApi
 {
-    public class BlogService : Greeter.GreeterBase
+    public class BlogService : BlogProto.BlogProtoBase
     {
-        private readonly ILogger<BlogService> _logger;
-        public BlogService(ILogger<BlogService> logger)
+        private readonly ILogger<BlogService> logger;
+        private readonly BloggingContext bloggingContext;
+
+        public BlogService(ILogger<BlogService> logger, BloggingContext bloggingContext)
         {
-            _logger = logger;
+            this.logger = logger;
+            this.bloggingContext = bloggingContext;
         }
 
-        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        public override async Task<Blogs> GetBlogs(Empty request, ServerCallContext context)
         {
-            return Task.FromResult(new HelloReply
-            {
-                Message = "Hello " + request.Name
-            });
+            var query = await bloggingContext.Blogs
+                    .Include(i => i.Posts)
+                    .ToListAsync();
+            
+            var blogs = new Blogs();
+            blogs.BlogsData.AddRange(query);
+            
+            return blogs;
         }
     }
 }
